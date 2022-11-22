9f75029aa752a2c33995ad3dd34ab06fa9f83f72
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-11-23 06:37:56 +0700

    Add files via upload

---
+++ kode.49.asosiatif.py
@@ -0,0 +1,22 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+print('Isi nilai-nilai Dengan Benar ')
+a = int(input("Masukan Nilai A  : "))
+b = int(input("Masukan Nilai B  : "))
+c = int(input("Masukan Nilai C  : "))
+
+# Mengkonversi
+hasilplus = a + b + c
+hasilkali = a * b * c
+
+# Menampilkan Hasil
+print('================================================================================')
+print('Jika Penjumlahan (' ,a, ' + ' ,b, ')+ ' ,c, ' = ' ,a, ' + (' ,b, ' + ' ,c, ') = d ')
+print('Maka Maka Hasilnya = ',hasilplus)
+print('================================================================================')
+print('Jika Perkalian (' ,a, ' x ' ,b, ')x ' ,c, ' = ' ,a, ' x (' ,b, ' x ' ,c, ') = d ')
+print('Maka Maka Hasilnya = ',hasilkali)
+print('================================================================================')
